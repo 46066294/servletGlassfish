50ce6e9510a22770fffc47d3f6cb86126a78465a
46066294 <46066294p@iespoblenou.org>
2016-05-23 19:30:32 +0200

    finalitzat

--- src/XifratCesar.java
+++ src/XifratCesar.java
@@ -19,77 +19,57 @@ public class XifratCesar extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String valueString = req.getParameter("value");
+
+        String strMissatge = req.getParameter("missatge");
 
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
 
         out.println("<html>\n" +
                 "  <head>\n" +
-                "    <script src=\"https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js\"></script>\n" +
-                "    <script src=\"https://cdn.firebase.com/js/client/2.2.4/firebase.js\"></script>\n" +
-                "    <script src=\"https://cdn.firebase.com/libs/angularfire/1.1.3/angularfire.min.js\"></script>\n" +
-                "\n" +
                 "    <title>ServidorXifratCesar</title>\n" +
-                "\n" +
-                "    <!-- Importamos Bootstrap  -->\n" +
-                "    <link rel=\"stylesheet\" href=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\">\n" +
-                "    <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js\"></script>\n" +
-                "    <script src=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js\"></script>\n" +
-                "\n" +
-                "    <!-- Barra de navegación -->\n" +
-                "    <nav class=\"navbar navbar-default\">\n" +
-                "      <div class=\"container-fluid\">\n" +
-                "\n" +
-                "        <!-- Brand and toggle get grouped for better mobile display -->\n" +
-                "        <div class=\"navbar-header\">\n" +
-                "          <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\" aria-expanded=\"false\">\n" +
-                "            <span class=\"sr-only\">Toggle navigation</span>\n" +
-                "            <span class=\"icon-bar\"></span>\n" +
-                "            <span class=\"icon-bar\"></span>\n" +
-                "            <span class=\"icon-bar\"></span>\n" +
-                "          </button>\n" +
-                "          <a class=\"navbar-brand\" href=\"./index.jsp\">Marc Cano | Poblenou </a>\n" +
+                "        <div class=\"container-fluid\">\n" +
+                "          <a class=\"navbar-brand\" href=\"./index.jsp\"><--Enrere </a>\n" +
                 "        </div>\n" +
                 "\n" +
-                "      </div>\n" +
-                "    </nav>\n" +
                 "  </head>\n" +
+
                 "<body>\n" +
-                "   <center>\n" +
                 "       <h1>Xifrat Cesar</h1>\n" +
-                "           <p>Introdueix text per xifrat Cesar!: " + valueString + "</p>"
+                "           <p>Text original:\n" + strMissatge + "</p>"
         );
 
-        // Escribimos en cada linea encriptado con un indice diferente
-        for (int iterador = 1; iterador < 26; iterador++) {
-            out.println("<li>" + encriptarCesar(valueString, iterador) + "</p></li>");
+        out.println("Missatge xifrat:");
+
+        for (int i = 0; i < 26; i++) {
+            if(i != 0)
+                out.println("<li>" + encryptCesar(strMissatge, i) + "</li>");
         }
 
-        out.println(" </center>\n" +
-                "  </body>\n" +
-                "</html>");
+        out.println("</body></html>");
     }
+                                                        //comprobacio lletres en alfbt
+    private String encryptCesar(String textOriginal, int indice) {
 
-    private String encriptarCesar(String textoAEncriptar, int indice) {
+        char textXifrat[];
+        textXifrat = textOriginal.toCharArray();
 
-        char fraseCifrada[];
-        fraseCifrada = textoAEncriptar.toCharArray();
+        for (int contChar = 0; contChar < textOriginal.length(); contChar++) {
+            for (int i = 0; i < indice; i++) {
 
-        for (int iteradorCaracteres = 0; iteradorCaracteres < textoAEncriptar.length(); iteradorCaracteres++) {
-            for (int iteradorAlfabeto = 0; iteradorAlfabeto < indice; iteradorAlfabeto++) {
-
-                if ((fraseCifrada[iteradorCaracteres] >= 65 && fraseCifrada[iteradorCaracteres] < 90) || (fraseCifrada[iteradorCaracteres] >= 97 && fraseCifrada[iteradorCaracteres] < 122)) {
-                    fraseCifrada[iteradorCaracteres]++;
+                if ((textXifrat[contChar] >= 65 && textXifrat[contChar] < 90)
+                        || (textXifrat[contChar] >= 97 && textXifrat[contChar] < 122)) {
+                    textXifrat[contChar]++;
                 }
-                else if (fraseCifrada[iteradorCaracteres] == 90)
-                    fraseCifrada[iteradorCaracteres] = 'A';
-                else if (fraseCifrada[iteradorCaracteres] == 122)
-                    fraseCifrada[iteradorCaracteres] = 'a';
+                else if (textXifrat[contChar] == 122)
+                    textXifrat[contChar] = 'a';
+                else if (textXifrat[contChar] == 90)
+                    textXifrat[contChar] = 'A';
             }
         }
-        textoAEncriptar = String.valueOf(fraseCifrada);
-        return textoAEncriptar;
+
+        textOriginal = String.valueOf(textXifrat);
+        return textOriginal;
     }
 
 }
--- web/index.jsp
+++ web/index.jsp
@@ -7,48 +7,22 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-<head>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
-    <script src="https://cdn.firebase.com/js/client/2.2.4/firebase.js"></script>
-    <script src="https://cdn.firebase.com/libs/angularfire/1.1.3/angularfire.min.js"></script>
-
-    <title>ServidorXifratCesar</title>
-
-    <!-- Importamos Bootstrap  -->
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
-    <!-- Barra de navegación -->
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand">Marc Cano | Poblenou </a>
+    <head>
+        <title>ServidorXifratCesar</title>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <a class="navbar-brand">Marc Cano ECAIB --M06-- </a>
             </div>
+        </nav>
+    </head>
 
-        </div>
-    </nav>
-</head>
+    <body>
+        <h1>Xifrat Cesar</h1>
+        <p>Introdueix text per xifrat Cesar</p>
 
-<body>
-<center>
-
-    <h1>Xifrat Cesar</h1>
-    <p>Introdueix text per xifrat Cesar</p>
-
-    <form action="MarcCano" method="post">
-        <input name="value" type="text"/>
-        <input type="submit" value="Enviar"/>
-    </form>
-
-</center>
-</body>
+        <form action="MarcCano" method="post">
+            <input name="missatge" type="text"/>
+            <input type="submit" value="Encriptar Cesar"/>
+        </form>
+    </body>
 </html>
